6755ba1ebe75a3fae011b90a0fedfbb9b46d58a3
eastoncrafter <74274138+eastoncrafter@users.noreply.github.com>
2022-04-20 09:08:09 -0400

    install nginx

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #make sure to run as sudo
-apt install -y lsb-release wget apt-transport-https wget curl git -y
+apt update
+apt install lsb-release wget apt-transport-https wget curl git -y
+apt install nginx -y
 wget -O /usr/share/keyrings/matrix-org-archive-keyring.gpg https://packages.matrix.org/debian/matrix-org-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/matrix-org-archive-keyring.gpg] https://packages.matrix.org/debian/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/matrix-org.list
 apt update
