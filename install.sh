1ad5a5384ee3460f8142561f4da23d278103964f
eastoncrafter <74274138+eastoncrafter@users.noreply.github.com>
2022-04-19 22:03:36 -0400

    edits

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 #make sure to run as sudo
 apt install -y lsb-release wget apt-transport-https wget curl git -y
 wget -O /usr/share/keyrings/matrix-org-archive-keyring.gpg https://packages.matrix.org/debian/matrix-org-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/matrix-org-archive-keyring.gpg] https://packages.matrix.org/debian/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/matrix-org.list
+apt update
+apt install matrix-synapse-py3
